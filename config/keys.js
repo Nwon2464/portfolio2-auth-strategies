8919a202a94b369f3d6a60dc91330b55608f4178
dnjsskarb7 <64752079+dnjsskarb7@users.noreply.github.com>
2020-08-05 01:12:28 +0900

    a

---
+++ config/keys.js
@@ -0,0 +1,38 @@
+module.exports = {
+  google: {
+    clientId:
+      "219815912128-bldp409j72pld6pcbmrqaj8e9c8lgfsc.apps.googleusercontent.com",
+    clientSecret: "-W-Gd6l8waXGVncTMe3Kc0ie",
+  },
+
+  session: {
+    cookieKey: "abcdefg",
+  },
+  facebook: {
+    clientId: "719812728589947",
+    clientSecret: "74d1c59a40e3f937b99dec5e4825eed4",
+  },
+  spotify: {
+    clientId: "f1669893d90d4addad92e38499962ccf",
+    clientSecret: "499b1bb63a6e4bc7a41849c83e981e84",
+  },
+  twitch: {
+    clientId: "6g0e69w46upnufsnxjvdarcxxsoiaj",
+    clientSecret: "2zaza37zqzn9wmpuqzulyhflrxibc3",
+  },
+  github: {
+    clientId: "4d1c1693eb43feb51bbc",
+    clientSecret: "a8d5fb159f1c555ff09bcbb04ecc05ac6bc7f44c",
+  },
+  amazon: {
+    clientId: "amzn1.application-oa2-client.ad1065bd0e5e4ab091cdfbcf5e0688f0",
+    clientSecret:
+      "9e0a5809ee6ab2e36546c4f9b5605c7eeea72a650496a54ec438dfb6358a1b27",
+  },
+  instagram: {
+    clientId: "750788502400787",
+    clientSecret: "789e07d31c3a523d3bef8abe16f1e187",
+  },
+};
+
+
